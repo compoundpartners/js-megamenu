fc1c4e76d0cba775f68a155643639e16d08c6eef
Oliver Ashford <oliver@compoundpartners.co.uk>
2019-06-12 10:19:22 +0100

    Added Bootstrap4LinkPlugin to allowed plugins

--- js_megamenu/cms_plugins.py
+++ js_megamenu/cms_plugins.py
@@ -11,8 +11,12 @@ class MegamenuSectionPlugin(CMSPluginBase):
     render_template = 'megamenu/section.html'
     admin_preview = False
     allow_children = True
-    child_classes = ['TextPlugin', 'Bootstrap4BoxoutPlugin',
-        'JSLinkListPlugin', 'Bootstrap4CollapsePlugin', 'PromoUnitPlugin']
+    child_classes = ['TextPlugin',
+        'Bootstrap4BoxoutPlugin',
+        'JSLinkListPlugin',
+        'Bootstrap4CollapsePlugin',
+        'Bootstrap4LinkPlugin', 
+        'PromoUnitPlugin']
 
     def render(self, context, instance, placeholder):
         context.update({
